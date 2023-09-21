dc3a4126ed28039eb4f2872194c216937287a127
HARSH KUMAR SINHA <128177288+Harsh-kumar-sinha-427742@users.noreply.github.com>
2023-09-21 23:16:47 +0530

    Delete AtmOpeartion.java

--- AtmOpeartion.java
+++
@@ -1,8 +0,0 @@
-package ATM_machine;
-
-public interface AtmOpeartion {
-	public void viewBalance();
-	public void withdrawAmt(double withdrawAmt);
-	public void depositAmt(double depositAmt);
-	public void viewMiniStatement();
-}
